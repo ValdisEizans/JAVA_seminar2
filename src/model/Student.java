0af44d5f6f87276b9ab38a0dfe9545e7c039eab1
ValdisEizans <valdis.eizans@gmail.com>
2026-02-25 11:16:02 +0200

    Edit: setters added to Studend class

--- src/model/Student.java
+++ src/model/Student.java
@@ -3,14 +3,17 @@ package model;
 //Modelu klase
 public class Student {
 	//Mainigie
-	private long studID;
+	private long studId;
 	private String name;
 	private String surname;
 	private String personCode;
 	
+	//Paligmainigais bez setera/getera
+	private static long counter = 0;
+	
 	//Geteri 
 	public long getStudID() {
-		return studID;
+		return studId;
 	}
 	public String getName() {
 		return name;
@@ -23,8 +26,35 @@ public class Student {
 	}
 	
 	//Seteri
-	void setStudID(long studID) {
-		this studID -> studID;
+	public void setStudID() {
+		studId = counter;
+		counter++;
+	}
+	public void setName(String inputName) {
+		if((inputName != null) && (!inputName.isEmpty()) && (inputName.matches("[A-Z]{1}[a-z]{2,15}([*]{1}[A-Z]{1}[a-z]{2,15})?")) ) {
+			name = inputName;
+		}
+		else{
+			name = "Unknown";
+		}
+	}
+	public void setSurname(String inputSurname) {
+		if((inputSurname != null) && (!inputSurname.isEmpty()) && (inputSurname.matches("[A-Z]{1}[a-z]{2,25}([-]{1}[A-Z]{1}[a-z]{2,25})?")) ) {
+			surname = inputSurname;
+		}
+		else{
+			surname = "Unknown";
+		}
+	}
+	public void setPersonCode(String inputPersoncode) {
+		//TODO regex maska dienam un menesiem
+		if ((inputPersoncode != null) && (!inputPersoncode.isEmpty()) && (inputPersoncode.matches("[0-9]{6}[-]{1}[0-9]{5}")) ) {
+			personCode = inputPersoncode;
+		}
+		else {
+			personCode = "Unknown";
+		}
+		
 	}
 	
 	
