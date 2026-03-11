2a35f61ea03ab4afae8d34cda853a598d448c62a
ValdisEizans <valdis.eizans@gmail.com>
2026-03-11 11:55:09 +0200

    Edit: service class updated with arrays

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -1,5 +1,8 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import model.Course;
 import model.Grade;
 import model.Professor;
@@ -7,48 +10,64 @@ import model.Student;
 import model.enums.ProfDegree;
 
 public class MainService {
+	
+	private static ArrayList<Student> allStudents = new ArrayList<Student>();
+	private static ArrayList<Professor> allProfessors = new ArrayList<Professor>();
+	private static ArrayList<Course> allCourses = new ArrayList<Course>();
+	private static ArrayList<Grade> allGrades = new ArrayList<Grade>();
+	
 
 	public static void main(String[] args) {
 		//datu tips, nosaukums, new un tad konstruktors
 		System.out.println("--------------- Studenti: ------------------");
 		System.out.println();
 		Student stud1 = new Student();
-		System.out.println(stud1);
 		Student stud2 = new Student("Guntis","Eglitis","785412-96541");
-		System.out.println(stud2);
 		Student stud3 = new Student("Kristaps","Abols","123456-78541");
-		System.out.println(stud3);
+		allStudents.add(stud1);
+		allStudents.add(stud2);
+		allStudents.add(stud3);
+		
+		for (Student tempS : allStudents) {
+			System.out.println(tempS);
+		}
 
 		System.out.println();
 		System.out.println("--------------- Profesori: ------------------");
 		System.out.println();
 		Professor prof1 = new Professor();
-		System.out.println(prof1);
 		Professor prof2 = new Professor("Agris","Ziemelis","125874-98745",ProfDegree.barchelor);
-		System.out.println(prof2);
 		Professor prof3 = new Professor("Andris","Gobzems","125784-98745",ProfDegree.phd);
-		System.out.println(prof3);
+		allProfessors.addAll(Arrays.asList(prof1,prof2,prof3));
+
+		
+		for (Professor tempP : allProfessors) {
+			System.out.println(tempP);
+		}
 		
 		System.out.println();
 		System.out.println("--------------- Kursi: ------------------");
 		System.out.println();
 		Course course1 = new Course();
-		System.out.println(course1);
 		Course course2 = new Course("Augstaka matematika",(byte)32,prof3);
-		System.out.println(course2);
 		Course course3 = new Course("Ekonomika",(byte)16,prof2);
-		System.out.println(course3);
+		allCourses.addAll(Arrays.asList(course1,course2,course3));
+		
+		for (Course tempC : allCourses) {
+			System.out.println(tempC);
+		}
 		
 		System.out.println();
 		System.out.println("--------------- Atzimes: ------------------");
 		System.out.println();
 		Grade grade1 = new Grade();
-		System.out.println(grade1);
 		Grade grade2 = new Grade((byte)8,stud2,course2);
-		System.out.println(grade2);
 		Grade grade3 = new Grade((byte)7,stud2,course3);
-		System.out.println(grade3);
+		allGrades.addAll(Arrays.asList(grade1,grade2,grade3));
 
-	}
+		for (Grade tempG : allGrades) {
+			System.out.println(tempG);
+		}
+}
 
 }
